d27d23b42b8375f6dc197664f74b8e7c29afa626
tt <126308179+xxxxt3@users.noreply.github.com>
2026-04-23 07:08:46 +0800

    Add main function to process range queries
    Implement main function to read values and count out-of-range elements.

---
+++ Matiji/mc0549.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+    int main(){
+        int n,m,a[10005],l,r,p=0;
+        scanf("%d %d",&n,&m);
+        for(int i=1;i<=n;i++){
+            scanf("%d",&a[i]);
+        }
+        for(int j=1;j<=m;j++){
+            scanf("%d %d",&l,&r);
+            for(int i=1;i<=n;i++){
+                if(a[i]<l||a[i]>r){
+                    p++;
+                }
+            }
+        }
+        printf("%d",p%10007);
+    }
